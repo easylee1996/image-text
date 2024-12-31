866f78a88342103080febae8fc39b5fc96cd3a3c
easylee <2452241217@qq.com>
2024-12-31 10:44:07 +0800

    feat: 更新封面图

--- cover-generate/src/App.vue
+++ cover-generate/src/App.vue
@@ -171,7 +171,12 @@ onMounted(() => {
 
     textContent.value = params.title
 
+    // 判断电脑系统版本, mac无头模式，字体排版样式不一样，导致换行的字数不一样
+    const userAgent = navigator.userAgent.toLowerCase()
+    const isMac = userAgent.includes('mac') ? true : false
+
     const filterCoverTextStyleList = coverTextStyleList.filter(item => {
+        if (isMac) return !item.notLength.includes(textContent.value.length - 1)
         return !item.notLength.includes(textContent.value.length)
     })
 
